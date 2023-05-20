cb035b303412f8cd0cee7973fb594d28e4a9b90e
Jasper Macaraeg <81204548+mjasper30@users.noreply.github.com>
2023-05-20 20:34:31 +0800

    Add files via upload

---
+++ moisture-sensor/moisture-sensor.ino
@@ -0,0 +1,18 @@
+// tinkercad: not yet tried
+// arduino: working
+
+int moistureValue;
+float moisture_percentage;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  moistureValue = analogRead(A0);
+  moisture_percentage = ((moistureValue/539.00)*100);
+  Serial.print("\n Moisture Value: ");
+  Serial.print(moisture_percentage);
+  Serial.print("%");
+  delay(1000);
+}
